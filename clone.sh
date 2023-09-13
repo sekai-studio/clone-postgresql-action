9adcba1bc7d87faade5d59b61369b0a7acd6ba13
Antoine Roy <antoine.roy77@gmail.com>
2023-09-13 14:54:20 +0200

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -7,4 +7,4 @@ PGPASSWORD=$4 pg_dump -Fc -U $3 -h $1 -p $2 $5 > db.dump
 PGPASSWORD=$9 psql -U $8 -h $6 -p $7 -d ${10} -c "DROP SCHEMA IF EXISTS ${11} CASCADE;"
 
 # restore the database
-PGPASSWORD=$9 pg_restore -U $8 -h $6 -p $7 -d ${10} db.dump --clean
+PGPASSWORD=$9 pg_restore -U $8 -h $6 -p $7 -d ${10} db.dump
