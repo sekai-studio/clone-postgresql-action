48598e69e7e15d0c29475844ca1bb7a449e2d5cd
Antoine Roy <antoine.roy77@gmail.com>
2023-09-13 14:17:33 +0200

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -4,12 +4,14 @@
 PGPASSWORD=$4 pg_dump -Fc -U $3 -h $1 -p $2 $5 > db.dump
 
 # Drop schema
-PGPASSWORD=$9 psql -U $8 -h $6 -p $7 -d ${10} "DO $$ DECLARE
+PGPASSWORD=$9 psql -U $8 -h $6 -p $7 -d ${10} << SQL
+DO $$ DECLARE
     r RECORD;
 BEGIN
     FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = ${11}) LOOP
         EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
     END LOOP;
-END $$;"
+END $$;
+SQL
 # restore the database
 PGPASSWORD=$9 pg_restore -U $8 -h $6 -p $7 -d ${10} db.dump --clean
