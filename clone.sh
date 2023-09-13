1ea4b74e7c75955400beca83fa5d0ead33c4d956
Antoine Roy <antoine.roy77@gmail.com>
2023-09-13 15:04:07 +0200

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -6,5 +6,8 @@ PGPASSWORD=$4 pg_dump -Fc -U $3 -h $1 -p $2 $5 > db.dump
 # Drop schema
 PGPASSWORD=$9 psql -U $8 -h $6 -p $7 -d ${10} -c "DROP SCHEMA IF EXISTS ${11} CASCADE;"
 
+# Crate back schema public
+PGPASSWORD=$9 psql -U $8 -h $6 -p $7 -d ${10} -c "CREATE SCHEMA console;" -c "ALTER SCHEMA console OWNER TO $8;" -c "SET default_tablespace = '';" -c "SET default_table_access_method = heap;"
+
 # restore the database
 PGPASSWORD=$9 pg_restore -U $8 -h $6 -p $7 -d ${10} db.dump
