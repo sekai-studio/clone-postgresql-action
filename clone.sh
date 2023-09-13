628198c121890066daa9d58da6009186d556b846
Antoine Roy <antoine.roy77@gmail.com>
2023-09-13 14:43:10 +0200

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -4,14 +4,7 @@
 PGPASSWORD=$4 pg_dump -Fc -U $3 -h $1 -p $2 $5 > db.dump
 
 # Drop schema
-PGPASSWORD=$9 psql -U $8 -h $6 -p $7 -d ${10} << SQL
-DO $$ DECLARE
-    r RECORD;
-BEGIN
-    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = ${11}) LOOP
-        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(r.tablename) || ' CASCADE';
-    END LOOP;
-END $$;
+PGPASSWORD=$9 psql -U $8 -h $6 -p $7 -d ${10} -c "DROP SCHEMA IF EXISTS ${11} CASCADE;"
 SQL
 # restore the database
 PGPASSWORD=$9 pg_restore -U $8 -h $6 -p $7 -d ${10} db.dump --clean
